3b4ef536942a2e16d0c335e5e1c0154cbf54874e
schu38 <sandy.chu.40@berkeley.edu>
2018-01-28 21:06:35 -0800

    Update morelines2.js

--- morelines2.js
+++ morelines2.js
@@ -13,5 +13,17 @@ function trDraw() {
   line (340, 0, random(width), random(height));
 }
 
-// code for c. 
+// unfinished code for c.
+function fancyDraw() {
+  var rise = random(100,250);
+  var run =  rise / 2;
+  var danke = 0;
+  while (danke < 10) {
+  	line (run, rise, rise, run);
+  danke = danke + 1;
+  }
+}
+fancyDraw();
+
+
   
