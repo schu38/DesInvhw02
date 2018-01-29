baea38241d53cb48220e6faa5f069f20699f82d9
schu38 <sandy.chu.40@berkeley.edu>
2018-01-28 20:55:18 -0800

    Create morelines-2.js

---
+++ morelines-2.js
@@ -0,0 +1,17 @@
+// code for a.
+
+function setup() {
+  createCanvas(340, 340);
+}
+
+function midDraw() {
+  line(170, 170, random(width), random(height));
+}
+// code for b.
+
+function trDraw() {
+  line (340, 0, random(width), random(height));
+}
+
+// code for c. 
+  
